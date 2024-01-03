8b67d589cf8e4fe0d0488bea06368d7c0d4fdb70
LEGION5DEJAN\tomin <tomincdejan@gmail.com>
2024-01-03 19:11:42 +0100

    dodan post method za zacetek snemanja

--- racGrafika_server.py
+++ racGrafika_server.py
@@ -18,13 +18,13 @@ def upload():
 
     data = request.json
     image_data = data['image']
+
     # Convert Base64 to binary
     image_binary = base64.b64decode(image_data.split(',')[1])
 
     # Generate a unique file name (e.g., using a timestamp)
     file_name = 'server_web_image.png'
     file_path = os.path.join(IMAGE_SAVE_PATH, file_name)
-    print("zivjo kostrun")
 
     # Save the image
     with open(file_path, 'wb') as file:
@@ -34,6 +34,17 @@ def upload():
     #producer.send('your_topic', image_binary)
 
     return jsonify({"status": "success", "file_saved": file_name})
+
+@app.route('/start_recording', methods=['POST'])
+def process_image():
+    data = request.json
+    #process_now = data.get('process_now', False)
+    process_now = data['flag']
+
+    print("dobil sem odgovor: ", process_now)
+
+
+    return jsonify({"status": "success", "message": "flag recieved"})
 
 if __name__ == '__main__':
 
